269b42743c40573dfa338d46d27fbf373c063da1
tomdevvn90 <tomdevvn90@gmail.com>
2023-11-23 13:42:15 +0700

    add style About page and add Box Icon Button block

---
+++ resources/blocks/be-box-icon-button/render.php
@@ -0,0 +1,16 @@
+<?php
+
+// create id attribute for specific styling
+$id = 'be-box-icon-btn' . $block['id'];
+
+// ACF field variables
+// $bgImage = (!empty(__get_field('bg__icon_box'))) ? __get_field('bg__icon_box') : '';
+
+
+
+?>
+<div id="<?php echo $id; ?>" class="be-box-icon-btn"> 
+   <div class="be-box-icon-btn-inner"> 
+       
+   </div>
+</div>
