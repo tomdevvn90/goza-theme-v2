2fbb2fe4616072cf6a57bce1904868489e6d8f02
tomdevvn90 <tomdevvn90@gmail.com>
2023-11-28 21:46:17 +0700

    update Be Fw Portfolio block

--- resources/blocks/be-fw-portfolio/render.php
+++ resources/blocks/be-fw-portfolio/render.php
@@ -1,7 +1,16 @@
 <?php
 // create id attribute for specific styling
-$id    = 'be-fw-portfolio-'.$block['id'];
-$align = $block['align'] ? 'align' . $block['align'] : '';
+$id     = 'be-fw-portfolio-'.$block['id'];
+$align  = $block['align'] ? 'align' . $block['align'] : '';
+$styles = [ ];
+
+$link_op            = __get_field('goza_link_color_op', 'option') ? : '';
+$link_color_op      = isset($link_op['link_color']) ? $link_op['link_color'] : '#d41b65';
+$typography_heading = __get_field('typography_heading', 'option');
+$heading_color_op   = isset($typography_heading['heading_color']) ? $typography_heading['heading_color'] : '#333';
+$typography_body    = __get_field('typography_body', 'option');
+$body_color_op      = isset($typography_body['body_color']) ? $typography_body['body_color'] : '#666';
+
 
 // option query fields
 $filter         = __get_field('filter_cate_fp_bl') ? : '';
@@ -11,42 +20,49 @@ $order          = __get_field('order_fp') ? __get_field('order_fp') : 'desc';
 $orderby        = __get_field('orderby_fp') ? __get_field('orderby_fp') : 'date';
 $loadmore_text  = __get_field('loadmore_text_fp') ? : __('View More', 'goza');
 
+// style options
+$hd_cl = __get_field('title_color_fp') ? : $heading_color_op;
+$ex_cl = __get_field('excerpt_color_fp') ? : $body_color_op;
+$bt_cl = __get_field('btn_color_fp') ? : $link_color_op;
 
-// layput
+$filter_style    = __get_field('filter_style_fp') ? : '';
+$load_more_style = __get_field('loadmore_style_fp') ?: 'btn-default';
 
-// $columns = __get_field('columns_projects_grid');
-// $columns_tablet = __get_field('columns_tablet_projects_grid');
-// $columns_mobile = __get_field('columns_mobile_projects_grid');
-// $column_gap = __get_field('column_gap_projects_grid');
-// $row_gap = __get_field('row_gap_projects_grid');
+if(isset($filter_style) && !empty($filter_style)){
+    $color_nm = $filter_style['color_normal'] ? : '#fff';
+    $bg_nm    = $filter_style['bg_normal'] ? : '#000';
+    $color_hv = $filter_style['color_hv'] ? : '#fff';
+    $bg_hv    = $filter_style['bg_hv'] ? : $link_color_op;
 
-// $columns_style = !empty( $columns )? '--columns-project-grid:'.$columns.';' : '';
-// $columns_tablet_style = !empty( $columns_tablet )?'--columns-project-grid-tablet:'.$columns_tablet.';' : '';
-// $columns_mobile_style = !empty( $columns_mobile )?'--columns-project-grid-mobile:'.$columns_mobile.';' : '';
-// $column_gap_style = !empty( $column_gap )? '--column-project-grid-gap:'.$column_gap.'px;' :  ( ( $column_gap === '0' )? '--column-project-grid-gap:'.$column_gap.'px;' : '' );
-// $row_gap_style = !empty( $row_gap )? '--row-project-grid-gap:'.$row_gap.'px;' : ( ( $row_gap === '0' )? '--column-project-grid-gap:'.$row_gap.'px;' : '' );
+    $styles['--filter-cl-nm'] = $color_nm;
+    $styles['--filter-bg-nm'] = $bg_nm;
+    $styles['--filter-cl-hv'] = $color_hv;
+    $styles['--filter-bg-hv'] = $bg_hv;
+}
 
-// $columns_variables = $columns_style.$columns_tablet_style.$columns_mobile_style.$column_gap_style.$row_gap_style;
+// layout options
+$columns        = __get_field('columns_fp') ? : '3';
+$columns_tablet = __get_field('columns_tablet_fp') ? : '2';
+$columns_mobile = __get_field('columns_mb_fp') ? : '1';
+$column_gap     = __get_field('column_gap_fp') ? : '20';
+$row_gap        = __get_field('row_gap_projects_grid') ? : '20';
 
-// // styles
-// $bg_color = __get_field('bg_color_projects_grid');
-// $title_color = __get_field('title_color_projects_grid');
-// $excerpt_color = __get_field('excerpt_color_projects_grid');
-// $button_color = __get_field('button_color_projects_grid');
 
-// $bg_color_style = !empty( $bg_color )? '--bg-color-project-grid:'.$bg_color.';' : '';
-// $title_color_style = !empty( $title_color )? '--title-color-project-grid:'.$title_color.';' : '';
-// $excerpt_color_style = !empty( $excerpt_color )? '--excerpt-color-project-grid:'.$excerpt_color.';' : '';
-// $button_color_style = !empty( $button_color )? '--btn-color-project-grid:'.$button_color.';' : '';
+$styles['--columns']    = $columns;
+$styles['--columns-tb'] = $columns_tablet;
+$styles['--columns-mb'] = $columns_mobile;
+$styles['--column-gap'] = $column_gap . 'px';;
+$styles['--row-gap']    = $row_gap . 'px';
+$styles['--hd-color']   = $hd_cl;
+$styles['--ex-color']   = $ex_cl;
+$styles['--btn-color']  = $bt_cl;
 
-// $color_variables = $bg_color_style.$title_color_style.$excerpt_color_style.$button_color_style;
-// // option query fields
-// $posts_per_page = !empty( __get_field('posts_per_page_projects_grid') )? __get_field('posts_per_page_projects_grid') : get_option('posts_per_page');
-// $order = __get_field('order_projects_grid') ? __get_field('order_projects_grid') : 'desc';
-// $orderby = __get_field('orderby_projects_grid') ? __get_field('orderby_projects_grid') : 'date';
-// $loadmore = __get_field('loadmore_projects_grid') ? __get_field('loadmore_projects_grid') : false;
-// $loadmore_type = __get_field('loadmore_type_projects_grid') ? __get_field('loadmore_type_projects_grid') : 'default';
-// $loadmore_text = __get_field('loadmore_text_projects_grid') ? __get_field('loadmore_text_projects_grid') : __('View More', 'goza');
+// Combining styles into a single string using semicolons
+$styleString = '';
+foreach ($styles as $key => $value) {
+    $styleString .= "$key: $value; ";
+}
+$styleString = rtrim($styleString, '; '); 
 
 $paged = get_query_var( 'paged' )? get_query_var( 'paged' ) : 1;
 
@@ -68,6 +84,7 @@ $max_num_pages = $the_query->max_num_pages;
     data-posts-per-page = "<?= $posts_per_page ?>"
     data-orderby = "<?= $orderby ?>"
     data-order   = "<?= $order ?>"
+    style="<?= esc_attr($styleString); ?>"
 >
     <div class="container"> 
         <div class="be-fw-portfolio-block-inner"> 
@@ -107,13 +124,19 @@ $max_num_pages = $the_query->max_num_pages;
             
             <?php if ( $loadmore && $max_num_pages > 0 ):?>
                 <div class="be-fw-portfolio-block--loadmore"  data-aos="fade-up" data-aos-duration="1000">
-                    <a href="#" class="be-fw-portfolio-block--loadmore-btn"
+                    <a href="#!" class="be-fw-portfolio-block--loadmore-btn btn <?= esc_attr($load_more_style) ?>"
                         data-page="1"
                         data-max-page ="<?= esc_attr( $max_num_pages ) ?>"
                         data-term ="all"
                     >
                         <?= $loadmore_text ?>
+                        <?php if ($load_more_style == 'btn-water') { ?>
+                            <svg class="wgl-dashes inner-dashed-border animated-dashes">
+                                <rect rx="0%" ry="0%"> </rect>
+                            </svg>
+                        <?php } ?>
                     </a>
+
                 </div>
             <?php endif; ?>
         </div>
