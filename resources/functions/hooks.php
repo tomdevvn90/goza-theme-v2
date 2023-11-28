ccbcbef6cd4a9bcfe085aaae0fe9d4a846a471aa
buitantoan <toanbuitan@gmail.com>
2023-11-28 16:43:56 +0700

    update style page shop woocommerce

--- resources/functions/hooks.php
+++ resources/functions/hooks.php
@@ -368,3 +368,26 @@ function goza_woocommerce_before_main_content_single_product_func()
 	<?php } ?>
 <?php
 			}
+
+// Shop page
+
+add_filter( 'woocommerce_loop_add_to_cart_link', 'goza_woocommerce_loop_add_to_cart_link_func', 10, 3 );
+function goza_woocommerce_loop_add_to_cart_link_func( $add_to_cart_html, $product, $args ){
+
+	$icon = '<span class="icon">
+		<svg class="svg-icon" width="16" height="16" fill="#FFFFFF" aria-hidden="true" role="img" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="-35 0 512 512.00102">
+			<path d="m443.054688 495.171875-38.914063-370.574219c-.816406-7.757812-7.355469-13.648437-15.15625-13.648437h-73.140625v-16.675781c0-51.980469-42.292969-94.273438-94.273438-94.273438-51.984374 0-94.277343 42.292969-94.277343 94.273438v16.675781h-73.140625c-7.800782 0-14.339844 5.890625-15.15625 13.648437l-38.9140628 370.574219c-.4492192 4.292969.9453128 8.578125 3.8320308 11.789063 2.890626 3.207031 7.007813 5.039062 11.324219 5.039062h412.65625c4.320313 0 8.4375-1.832031 11.324219-5.039062 2.894531-3.210938 4.285156-7.496094 3.835938-11.789063zm-285.285157-400.898437c0-35.175782 28.621094-63.796876 63.800781-63.796876 35.175782 0 63.796876 28.621094 63.796876 63.796876v16.675781h-127.597657zm-125.609375 387.25 35.714844-340.097657h59.417969v33.582031c0 8.414063 6.824219 15.238282 15.238281 15.238282s15.238281-6.824219 15.238281-15.238282v-33.582031h127.597657v33.582031c0 8.414063 6.824218 15.238282 15.238281 15.238282 8.414062 0 15.238281-6.824219 15.238281-15.238282v-33.582031h59.417969l35.714843 340.097657zm0 0"></path>
+		</svg>
+	</span>';
+
+	$button_text = '<span class="text">'.$product->add_to_cart_text().'</span>';
+	$button = $icon . $button_text;
+	return sprintf(
+		'<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
+		esc_url( $product->add_to_cart_url() ),
+		esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
+		esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
+		isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
+		__( $button, 'goza' )
+	);
+}
